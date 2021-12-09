d9f94d280f6c4ffe0d4f81d5fdd500489fb84c8f
Felipe Vogel <fps.vogel@gmail.com>
2021-12-08 23:36:51 -0500

    Query the OpenAI API

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,16 +1,42 @@
 class StoriesController < ApplicationController
+  # TODO auto-growing textarea: https://gist.github.com/yunusemredilber/c0598e168ef643fa8e9876b78c447b91
+  # TODO rescue_from ActionDispatch::Cookies::CookieOverflow
+  #      with alert: "The story is too big to fit in the session cookie."
+  # TODO but ultimately, prompt to sign up for an account.
+
   DEFAULT_STORY = "It was the year 2121. It was a post-apocalyptic world. " \
     "Zombies crowded around my house. One started pounding at my front door. " \
-    "I huddled in the corner of my living room, too terrified to move."
+    "I huddled in the corner of my living room, too terrified to move. "
+  RESPONSE_SIZE = 50
+  RESPONSE_TEMPERATURE = 0.5
 
   def show
     @story = session[:story] || DEFAULT_STORY
   end
 
   def create
-    # TODO call OpenAI API here, and add on the results to the story.
-    temp_continuation = " And then they decided to go home."
-    session[:story] = params[:story] + temp_continuation
+    response = ai_response(params[:story])
+    session[:story] = params[:story] + response
     redirect_to root_path
+  end
+
+  private
+
+  def ai_response(prompt)
+    api_key = Rails.application.credentials.dig(:open_ai, :api_key)
+    client = OpenAI::Client.new(access_token: api_key)
+    raw_resp = client.completions(engine: "davinci",
+                                  parameters:
+                                    { prompt: prompt,
+                                      max_tokens: RESPONSE_SIZE,
+                                      temperature: RESPONSE_TEMPERATURE })
+    response = raw_resp.parsed_response["choices"].map { |c| c["text"] }.first
+    clean_up(response)
+  end
+
+  def clean_up(response)
+    response.gsub("\u00A0", " ") # non-breaking space
+            .strip
+            .gsub(/\s{2,}/, " ") + " "
   end
 end
--- spec/stories_spec.rb
+++ spec/stories_spec.rb
@@ -2,12 +2,12 @@ require "rails_helper"
 
 RSpec.describe "API-querying story page", type: :system do
   describe "story form" do
-    it "has a default pre-filled story" do
+    it "has a default start to the story" do
       visit root_path
       expect(find('textarea').text.length).to be > 0
     end
 
-    it "outputs more story" do
+    it "continues the story when submitted" do
       visit root_path
       default_story_length = find('textarea').text.length
       find('input[type="submit"]').click
