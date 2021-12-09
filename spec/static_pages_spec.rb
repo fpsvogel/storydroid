30d9176ab5c0bc120fef6cbd39c3101563bf3e9a
Felipe Vogel <fps.vogel@gmail.com>
2021-12-08 21:58:04 -0500

    Remove redundant tests

--- spec/static_pages_spec.rb
+++ spec/static_pages_spec.rb
@@ -2,19 +2,9 @@ require "rails_helper"
 
 RSpec.describe "Basic API-querying homepage", type: :system do
   describe "story form" do
-    it "has a text area" do
-      visit root_path
-      expect(page).to have_selector('textarea')
-    end
-
     it "has a default pre-filled story" do
       visit root_path
       expect(find('textarea').text.length).to be > 0
-    end
-
-    it "has a submit button" do
-      visit root_path
-      expect(page).to have_selector('input[type="submit"]')
     end
 
     it "outputs more story" do
