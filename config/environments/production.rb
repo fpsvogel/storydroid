172948a36951b6c8110215d8302d68ae628167b0
Felipe Vogel <fps.vogel@gmail.com>
2021-12-08 23:49:54 -0500

    Set host URL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  Rails.application.routes.default_url_options[:host] = "https://storydroid.herokuapp.com/"
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
